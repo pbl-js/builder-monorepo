be7544d68f967258655eeaa5f3ed1a1d053b8740
Paweł Miłczak <pawel.milczak@monogo.pl>
2021-11-17 16:16:26 +0100

    Add express and prisma

--- apps/builder-api/src/main.ts
+++ apps/builder-api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 
 const app = express();
@@ -11,7 +6,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to builder-api!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.BUILDER_API_PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
